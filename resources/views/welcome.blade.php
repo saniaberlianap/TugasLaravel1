91ac89a25507b9896fa81e3cb1eb31db2bfc094d
saniaberlianap <saniabrlnn@gmail.com>
2020-01-28 19:45:27 +0700

    TugasLaravel1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tugas Laravel 1 Menggunakan Tailwind</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+
+ <style>
+            html, body {
+                background-color: #D7BDE2;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+</style>
+
+<body class="mt-20">
+    <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+        <div class="sm:flex sm:items-center px-6 py-4">
+            <div class="text-center sm:text-left sm:flex-grow">
+                <div class="mb-4">
+                    <p class="text-xl leading-tight">Sania Berliana Putri</p>
+                    <p class="text-sm leading-tight text-grey-dark">TI - M4A _ 183140714111001</p>
+                </div>
+                <div>
+                    <button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Add</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
